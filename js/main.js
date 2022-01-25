c096e3241a985fdea1e6107237963e431353b212
SusanBae <90326272+devSusan@users.noreply.github.com>
2022-01-25 15:22:44 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -49,7 +49,7 @@ const API_KEY = "150b76ccd12f98527017c528bff588ba";
 function onGeoOk(position) {
   const lat = position.coords.latitude;
   const lon = position.coords.longitude;
-  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
